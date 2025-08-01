365a5b37bd0a47f5ecad74010b4c7d5729af651d
Aprameya A Rao <rao.aprameya1205@gmail.com>
2025-08-02 01:12:28 +0530

    Changes done in about us page 02.08.25.3

--- src/components/about/Connect.jsx
+++ src/components/about/Connect.jsx
@@ -7,8 +7,7 @@ const socialLinks = [
   { href: "https://instagram.com/bmsce_acm", icon: <FaInstagram size={28} /> },
   { href: "https://www.linkedin.com/company/bmsce-acm-student-chapter/", icon: <FaLinkedin size={28} /> },
   { href: "https://www.youtube.com/channel/UC-p5iI_OFm_n8-wI_u4a5xw", icon: <FaYoutube size={28} /> },
-  { href: "https://github.com/bmsce-acm", icon: <FaGithub size={28} /> },
-  { href: "mailto:bmsce.acm@bmsce.ac.in", icon: <FaEnvelope size={28} /> },
+  { href: "https://github.com/bmsce-acm", icon: <FaGithub size={28} /> }
   
 ];
 
@@ -21,13 +20,13 @@ const Connect = () => {
                 <img src="/img/contact-1.webp" className="contact-clip-path-1 absolute left-10 top-2 w-72 h-full opacity-30 hidden lg:block" alt="deco" />
                 <img src="/img/contact-3.webp" className="sword-man-clip-path absolute -right-8 top-10 w-96 h-full opacity-30 hidden lg:block" alt="deco" />
                 
-                <p className="mb-10 font-general text-[10px] uppercase">
+                <p className="mb-10 font-general text-[30px] uppercase">
                     Stay Connected
                 </p>
 
                 <AnimatedTitle
-                    title="Follow Our Journey,<br />Join Our Community"
-                    className="!md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
+                    title="Follow Our Journey <br />Join Our Community"
+                    className="md:text-[2.5rem] w-full font-zentry text-xl !font-black !leading-[.9]"
                 />
 
                 <div className="flex justify-center gap-8 md:gap-12 mt-16">
