ba71beddd043141ab7df5278125c96c760f02e3e
Hrithik875 <160313400+Hrithik875@users.noreply.github.com>
2025-09-18 23:46:25 +0530

    added images to all the events

--- src/components/Data2_event.jsx
+++ src/components/Data2_event.jsx
@@ -22,7 +22,7 @@ const eventData = [
         image: pos1,
         fullDescription: 'The workshop, led by Mr. Sachin Lodha, Chief Scientist at TCS Research, was a fun and engaging session that introduced the concept of algorithms in a very relatable way. It started with a creative comparison asking, “Are you a good cook?” Just like a recipe gives step-by-step instructions to make a dish, an algorithm gives step-by-step instructions to solve a problem. A good recipe, or algorithm, not only gives the correct result but does so quickly and with less effort. This idea was made even more memorable with a reference to Amitabh Bachchan making an omelet in the movie Coolie. Participants were then introduced to what an algorithm actually is: a systematic process that solves a problem in a limited number of steps. To make the learning interactive, two classic problem scenarios were discussed. In the Hamlet’s Ski Rental Problem, Hamlet has to decide whether to rent or buy skis each day, without knowing how long he will continue skiing. This introduced the idea of online algorithms and decision-making under uncertainty. In the Treasure Hunt Problem, a blindfolded person stands at the origin while a treasure is hidden somewhere on the X-axis. The challenge is to find a strategy that helps the person locate the treasure with the least amount of walking. This introduced the doubling strategy and important concepts like competitive ratio and the Beeza-Yates theorem. The session used storytelling and real-life examples to explain technical concepts, making the learning enjoyable and memorable for everyone who attended'
         , outcomes: "The workshop helped participants gain a clear understanding of what algorithms are and how they can be used to solve real-life problems. Through engaging examples like Hamlet’s ski rental dilemma and the treasure hunt scenario, they learned how to make smart decisions even when the future is uncertain. The session introduced useful strategies such as the Doubling Strategy and explained key ideas like online algorithms, competitive ratio, and cost optimization in a simple and relatable way. It also encouraged logical thinking and step-by-step problem-solving. Overall, the workshop sparked curiosity and interest in algorithm design and competitive programming, making the learning process both fun and meaningful.",
-        gallery: ["img/9.webp","img/10.webp"],
+        gallery: ["img/fwa-1.webp","img/fwa-2.webp"],
         location: '1st Floor, PG Block',
         attendees: '63',
     },
@@ -34,7 +34,7 @@ const eventData = [
         image: pos2,
         fullDescription:"Grandmaster’s Gambit 3.0, hosted on Chess.com, was a thrilling and fast-paced online chess experience like no other. This unique event brought a refreshing twist to traditional chess competitions by focusing on team-based play. Each team consisted of two players who joined forces, combining their skills, strategies, and communication to outplay their opponents. The tournament featured a series of matches played in multiple chess formats, including blitz, bullet, and rapid games. This variety tested each team’s adaptability and versatility, pushing players to think quickly, plan effectively, and adjust their playstyles depending on the format and opponent. No two matches were the same — making every round a new challenge and an opportunity to shine. Teamwork was at the heart of this competition. Success depended not just on individual skill but also on how well players communicated, supported, and synchronized with each other. Whether it was discussing opening strategies, coordinating attacks, or defending in tough positions, teams had to stay sharp and stay united throughout the event. At the end of the tournament, the team with the most overall victories across all games and formats was declared the champion. Along the way, there were thrilling plays, surprising upsets, and memorable moments of collaboration and creativity. Whether it was tactical brilliance, fast thinking, or dynamic coordination, this tournament became the perfect stage for participants to showcase their passion for chess and their strength as a team."
         , outcomes: "The event is designed to deliver an engaging and challenging experience that goes beyond traditional individual chess play. By incorporating a variety of game formats and requiring close coordination between teammates, the tournament will showcase the value of strategic collaboration and adaptability under pressure. Participants will walk away with enriched skills in communication, rapid decision-making, and tactical planning across different time controls and styles of play. The event will also strengthen the sense of community among chess enthusiasts by fostering sportsmanship, mutual respect, and camaraderie through team-based competition. Ultimately, the tournament will crown a champion team that not only excels in individual talent but also demonstrates exceptional synergy and versatility. This outcome will serve as a celebration of both strategic excellence and cooperative gameplay in the evolving landscape of online chess.",
-        gallery: [],
+        gallery: ["img/gmg-1.webp","img/gmg-2.webp","img/gmg-3.webp","img/gmg-4.webp"],
         location: 'AIML LAB 1',
         attendees: '32',
     },
@@ -47,7 +47,7 @@ const eventData = [
         fullDescription:
             "Duos Dash 3.0, organized by the BMSCE ACM Student Chapter during UTSAV 2025, the annual cultural fest of BMS College of Engineering, was a fast-paced and fun-filled event made for teams of two. The event had four unique rounds, each testing different skills like teamwork, creativity, and quick thinking. The first round, Bomb Diffusion, took place in Roblox, where one teammate gave instructions while the other worked to diffuse a virtual bomb. This round was all about communication and trust. In the second round, Fliptionary, one teammate drew in the air using a flipped screen, while the other had to guess the word—leading to lots of laughs and surprises. The third round, Movie Charades, had one teammate give a prompt to Google Gemini AI, and the other guessed the movie based on the AI-generated image. The final round, Draw & Dare, used UNO cards to assign fun tasks. Each team had three chances to draw a card and complete a dare based on its color, earning points as they went. Duos Dash 3.0 brought together tech, creativity, and teamwork in a fun and memorable way. It truly matched the energy and excitement of UTSAV 2025. "
         , outcomes: "All the participants left the event with a feeling of accomplishment after playing the games. They were all highly anticipating the results. These games fostered a sense of connection and strengthened the bond present between two friends. It also strengthened the teamwork and initiative of the BMSCE ACM Student Chapter beyond technical activities since we learnt what it is like to function as a group and complete our tasks.",
-        gallery: [],
+        gallery: ["img/dd-1.webp","img/dd-2.webp","img/dd-3.webp"],
         location: '2nd Floor Common Area, PJA Block',
         attendees: '86',
     },
@@ -62,7 +62,7 @@ const eventData = [
             'Round 2 was "The Barter of Relics," a card trading game with a twist. Players got cool-looking fictional cards based on Marvel and sci-fi themes, but they didn’t know how valuable each card really was. They had to trade using their gut, charm, and strategy. At the end, the hidden values of the cards were revealed, which changed everyone’s scores and decided who stayed in the game.\n\n' +
             'Round 3, "The Final Convergence," brought it all together with a riddle-solving race. Teams faced a series of connected riddles based on five deep themes. Solving each riddle required spending their hard-earned points. They could also use special power-ups to delay or confuse their rivals. The event ended with a thrilling sprint to solve the final mystery first.'
         , outcomes: "The event helped participants improve their skills in strategy, memory, logic, and teamwork. It offered a fun yet competitive atmosphere with a variety of game formats that kept everyone engaged. Players got the chance to interact, collaborate, and build connections, whether playing solo or in teams. It also pushed them to think creatively and stay flexible under pressure. Overall, the event set a solid base for future interactive activities that blend entertainment with thoughtful challenges.",
-        gallery: [],
+        gallery: ["img/co-1.webp","img/co-2.webp"],
         location: '2nd Floor PJA  Block, Common Area',
         attendees: '33',
     },
@@ -77,7 +77,7 @@ const eventData = [
             "Each challenge was released daily, and participants were encouraged to submit their solutions on the HackerRank platform. The event also featured a leaderboard, motivating participants to compete and improve their problem-solving capabilities. The final phase of the event presented harder problems, testing the endurance and coding proficiency of the remaining participants.\n\n" +
             "The event concluded successfully with the announcement of the top three participants, who demonstrated remarkable consistency and coding skills throughout the 15 days. The leaderboard topper at the end of the 15 days was awarded a course of their choice worth up to ₹700, while the 2nd and 3rd prize winners received free registration to the AI Agents Workshop organized by the BMSCE ACM Student Chapter."
         , outcomes: "The event successfully fostered a culture of consistent coding practice among students. Participants developed their problem-solving abilities, learned new algorithms, and enhanced their coding efficiency. Additionally, the competition spirit motivated participants to keep pushing their limits.",
-        gallery: ["yes"],
+        gallery: ["img/15doc-1.webp","img/15doc-2.webp","img/15doc-3.webp","img/15doc-4.webp"],
         location: 'HackerRank (Online)',
         attendees: '150',
     },
@@ -90,7 +90,7 @@ const eventData = [
         fullDescription:
             "Algothon was a competitive event, typically focused on algorithmic problem-solving or algorithmic trading. Participants, in teams, were presented with a series of questions or challenges that required them to apply their knowledge of algorithms, data structures, and programming skills. The structure of Algothon involved multiple rounds, and in this specific format, there were three rounds. In each round, participants worked to solve the given problems. For every question they answered correctly, they were awarded a certain number of points, which in this case was 10 points. The goal was to accumulate the highest score by correctly solving the maximum number of problems within the given time frame for each round."
         , outcomes: "The participant with the most points wins the Event.",
-        gallery: [],
+        gallery: ["img/algo-1.webp","img/algo-2.webp"],
         location: 'SMV Hall PG Block',
         attendees: '22',
     },
@@ -103,7 +103,7 @@ const eventData = [
         fullDescription:
             "Mr. Sikhar Patranabis shared insights on the journey of cryptography from theoretical foundations to practical implementations, highlighting challenges in bridging provable security with real-world deployment. The talk explored the fascinating field of cryptography as pursued by IBM Research India, where around 200 researchers were engaged in pioneering work on threat management, cloud infrastructure, secure data and AI handling, as well as decentralized trust. The central theme was Zero Knowledge Proofs (ZKPs)—a groundbreaking cryptographic protocol that allowed one party (the prover) to convince another (the verifier) that a certain statement was true, without disclosing any additional information beyond the truth of the statement itself. ZKPs were built on four core principles: completeness (if the statement was true, an honest verifier would be convinced), soundness (a dishonest provider could not convince the verifier of a false statement), zero knowledge (no knowledge beyond the assertion was revealed), and succinctness (the proofs were short and efficiently verifiable). These principles were brought to life through intuitive examples, such as the color blindness test involving red and green balls and the Ali Baba cave scenario, both of which demonstrated how knowledge could be proven without exposure. The talk further delved into ZKPs of knowledge, such as demonstrating possession of a secret key (e.g., discrete logarithm problems) without revealing it, and highlighted the importance of arithmetic circuits for representing complex relations in general-purpose ZKP systems. These circuits formed the foundation of modern ZKP protocols, including SNARKs (Succinct Non-Interactive Arguments of Knowledge), which offered compact proof sizes and low verification costs. The talk also connected these cryptographic advances to real-world applications, particularly in blockchain technology, where zk-rollups were used to improve scalability. By offloading computation and enabling succinct verification of state updates, zk-rollups helped reduce gas fees, increase transaction throughput, and maintain the integrity of blockchain systems. Overall, the talk emphasized how cryptographic innovation was not only theoretically rich but also practically vital for the future of secure computing and scalable decentralized systems."
         , outcomes: "Participants gained valuable insights into how theoretical cryptographic concepts are applied to real-world systems. The talk enhanced their understanding of advanced security protocols and encouraged exploration of research opportunities in cryptography and blockchain scalability.",
-        gallery: [],
+        gallery: ["img/esp-1.webp","img/esp-2.webp"],
         location: 'online',
         attendees: '30',
     },
@@ -136,7 +136,7 @@ const eventData = [
             "• Collaboration among chapters and proactive planning can lead to national recognition and awards.\n" +
             "• Regular documentation and social media presence significantly enhance a chapter’s visibility.\n" +
             "• The ACM India Council is highly supportive and open to student-led initiatives that create long-term impact.",
-        gallery: [],
+        gallery: ["img/townhall.webp"],
         location: 'Online',
         attendees: '125',
     },
@@ -149,7 +149,7 @@ const eventData = [
         fullDescription:
             "The event commenced with a welcome speech by Roshini, setting the stage for an engaging session. This was followed by the traditional lighting of the lamp by the dignitaries, symbolizing the beginning of a new tenure for the chapter. Next, the Faculty Core members, Dr. Gowrishankar who is the Faculty Advisor and Dr. Seemanthini K who is the Faculty Sponsor were introduced, acknowledging their continued support and guidance. The Outgoing Office Bearers were then introduced and presented with mementos and certificates, recognizing their efforts and contributions to the chapter over the past year. Dr. Seemanthini K, was also presented with a memento and certificate for her constant support and contribution as the Faculty Sponsor of the chapter. The Outgoing Office Bearers delivered speeches highlighting the chapter’s progress and achievements. Harshavardhan presented the Secretary report for the year 2024 - 25 highlighting the major events conducted by the chapter during the year. Srujana provided an overview of the chapter’s annual achievements and growth of the chapter in terms of recognition and memberships. Followed by this Bhuvan presented the financial report for the tenure 2024 - 25, specifying the opening and closing balance amount and income and expenditure amount. Following this, Meghana introduced the newly appointed Office Bearers by presenting their certificates for serving as the Junior Coordinators and officially welcomed them into their roles. The Chairperson for the tenure 2025 - 26 then addressed the gathering, outlining the chapter’s vision and goals for the year 2025 - 26. This was followed by a speech from Dr. Seemanthini K, the Faculty Sponsor, who shared valuable insights and guidance for the chapter’s future endeavors.  The event concluded with a vote of thanks, expressing gratitude to the attendees, to the faculty, outgoing leadership and members for their contributions. The Annual General Meeting served as a significant milestone in shaping the future of the BMSCE ACM Student Chapter, paving the way for continued growth and success"
         , outcomes: "The BMSCE ACM Student Chapter Annual General Meeting 2025 was a great chance to look back at the chapter’s progress, welcome the new leaders, and plan for the future. The open forum allowed students and faculty to share their thoughts, making the chapter’s vision for the next year stronger.",
-        gallery: [],
+        gallery: ["img/agm-1.webp","img/agm-2.webp","img/agm-3.webp"],
         location: 'Auditorium - 2, PJA Block',
         attendees: '41',
     },
@@ -165,7 +165,7 @@ const eventData = [
             "• A total of 14 members contributed financially, while 5 members visited the orphanage to deliver the donation and spend time with the children.\n" +
             "• This initiative marked the beginning of the club’s commitment to community welfare, reinforcing the belief that small efforts can create a lasting impact."
         , outcomes: "Encouraged social responsibility and community engagement among members. Fostered a sense of empathy and contribution towards underprivileged children. Strengthened the teamwork and initiative of the BMSCE ACM Student Chapter beyond technical activities. Set a precedent for future community service initiatives under the SAMANVAYA series.",
-        gallery: [],
+        gallery: ["img/sam-1.webp"],
         location: '',
         attendees: '14',
     },
@@ -189,7 +189,7 @@ const eventData = [
 
             "The event encouraged participants to think critically, work together effectively, and enjoy a mix of technical and entertaining activities. The team with the highest score at the end of all three rounds was declared the winner."
         , outcomes: '"Trivi Arena" provided an excellent opportunity for participants to sharpen their problem-solving, teamwork, and creative thinking abilities in a dynamic and encouraging atmosphere. The event enabled every team member to play to their strengths, promoting a sense of unity and cooperative effort among all participants.',
-        gallery: [],
+        gallery: ["img/ta-1.webp","img/ta-2.webp","img/ta-3.webp"],
         location: 'PJ Block, 6th floor, Computer Science Data Science Lab',
         attendees: '50',
     },
@@ -219,7 +219,7 @@ const eventData = [
 
             "This iTeach initiative helped foster computational thinking at various levels, from basic pattern recognition to more advanced algorithmic concepts, providing both volunteers and students a chance to develop essential problem-solving skills."
         , outcomes: "The CS Pathshala program by the ACM India Council introduced computational thinking to young learners through engaging activities. Collaborating with the iTeach initiative by BMSCE Rotaract Club, the event combined technical education with personal growth. Volunteers honed skills like communication and teamwork while fostering critical thinking and problem-solving among students.",
-        gallery: [],
+        gallery: ["img/csp-1.webp","img/csp-2.webp"],
         location: 'Vidya Vinayaka Samsthe, Bengaluru',
         attendees: '100',
     },
