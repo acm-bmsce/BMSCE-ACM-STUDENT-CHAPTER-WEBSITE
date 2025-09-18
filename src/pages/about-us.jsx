3be3877672c2a221ce6c1ce4cacdee40bd21a9ac
Hrithik875 <160313400+Hrithik875@users.noreply.github.com>
2025-09-18 19:21:04 +0530

    added summit section to about us

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -3,19 +3,34 @@ import AboutACM from "../components/about/AboutACM";
 import AboutBMSCE_ACM from "../components/about/AboutBMSCE_ACM";
 import Journey from "../components/about/Journey";
 import GetInTouch from "../components/about/GetInTouch";
+import MagicBento from "../components/about/MagicBento";
 
 
 function AboutUs() {
   // You must add the "return" keyword here
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden bg-black">
-      
+
       <Hero />
       <AboutACM />
       <AboutBMSCE_ACM />
       <Journey />
-      <GetInTouch />
       
+
+      <MagicBento
+        textAutoHide={true}
+        enableStars={true}
+        enableSpotlight={true}
+        enableBorderGlow={true}
+        enableTilt={true}
+        enableMagnetism={true}
+        clickEffect={true}
+        spotlightRadius={300}
+        particleCount={12}
+        glowColor="52, 171, 235"
+      />
+      <GetInTouch />
+
     </main>
   );
 }
